99bfe52b2a3f5251e9892c2bfe730085922fe24e
Animesh <2025.cse18.animeshbag@gmail.com>
2024-10-13 15:09:58 +0530

    User auth added with register and login functionality

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from "express";
+import apiRouter from "./routers/index.js";
 
 const app = express();
+
+app.use(express.json());
+app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello VCrait");
--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 const mongo_uri = process.env.MONGO_URI;
+const jwt_secret = process.env.JWT_SECRET;
 
-export { port, mongo_uri };
+export { port, mongo_uri, jwt_secret };
